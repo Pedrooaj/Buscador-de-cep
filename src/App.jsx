23f78053f84afd01369d3e8921117ad944de09bb
Pedrooaj <pedroantonio5735@gmail.com>
2024-08-02 21:09:18 -0300

    loading update

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,25 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 function App() {
   const [cep, setCep] = useState('')
   const [address, setAddress] = useState({})
+  const [loading, setLoading] = useState(false)
 
 
   useEffect(() => {
 
     async function getCep() {
       if (cep.length === 8) {
+        setLoading(true)
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         const data = await response.json()
         setAddress(data)
       }
+      setTimeout(() => {
+        setLoading(false)
+      }, 500) 
+
     }
 
-    
+
     getCep()
 
   }, [cep])
@@ -38,17 +44,25 @@ function App() {
 
   }
 
+  const Conteudo = () => {
+    return (
+      <>
+        <div className='true'>{address.cep ? <><GiBrazil color='green' size={25} /> {address.cep}</> : ""}</div>
+        <div className='true'>{address.localidade ? <><FaCity color='yellow' size={25} /> {address.localidade}</> : ""}</div>
+        <div className='true'>{address.uf ? <><TbBuildingEstate color='blue' size={25} />{address.uf}</> : ""}</div>
+        <div className={address.bairro ? "true" : "false"}>{address.bairro ? <><SiOpenstreetmap size={25} /> {address.bairro}</> : ""}</div>
+        <div className='true'>{address.ddd ? <><BsFillTelephoneFill size={25} />{address.ddd}</> : ""}</div>
+      </>
+    )
+  }
+
 
   return (
     <div className="App">
       <div className='Div-Input'>
         <h1><FaMapMarked size={60} />Buscador de Cep </h1>
         <input placeholder='Digite um Cep' value={cep} onChange={(e) => handleInput(e)} type="number" />
-        <div className='true'>{address.cep? <><GiBrazil color='green' size={25} /> {address.cep}</>: ""}</div>
-        <div className='true'>{address.localidade? <><FaCity color='yellow' size={25} /> {address.localidade}</>: ""}</div>
-        <div className='true'>{address.uf? <><TbBuildingEstate color='blue' size={25} />{address.uf}</>: ""}</div>
-        <div className={address.bairro? "true": "false"}>{address.bairro? <><SiOpenstreetmap size={25} /> {address.bairro}</>: ""}</div>
-        <div className='true'>{address.ddd? <><BsFillTelephoneFill size={25} />{address.ddd}</>: ""}</div>
+        {loading ? <h2>Carregando...</h2> : <Conteudo />}
       </div>
     </div>
   )
